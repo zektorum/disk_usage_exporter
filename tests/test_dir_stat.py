1c8366c593d1b038f98588480ad443971155d79c
Murad Murtuzaliev <muradmurtuzaliev404@gmail.com>
2025-08-18 00:22:05 +0300

    Move search root to variable

--- tests/test_dir_stat.py
+++ tests/test_dir_stat.py
@@ -5,9 +5,11 @@ import pytest
 
 from disk_usage_exporter.disk_usage_exporter import get_dir_stat
 
+SEARCH_ROOT = os.path.join(os.getcwd(), "root")
+
 
 class TestDirStat:
-    @pytest.mark.parametrize('sample_directory_structure_with_data', [os.path.join(os.getcwd(), "root")], indirect=True)
+    @pytest.mark.parametrize('sample_directory_structure_with_data', [SEARCH_ROOT], indirect=True)
     def test_dir_size(self, sample_directory_structure_with_data: List[Dict[str, str]]):
         dirs = sample_directory_structure_with_data
 
